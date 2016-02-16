8de724bee1506e7424424ffdb2593f0b873fd075
HUYNH <charlie.huynh@atos.net>
2016-02-16 11:02:32 +0100

    Add endpoints implementations

--- client/js/controllers/configurations.js
+++ client/js/controllers/configurations.js
@@ -38,10 +38,12 @@ function Configurations($scope, $http) {
 
     $scope.activate = function(configuration) {
         if (configuration.status && configuration.status === status.available) {
-            /* TODO */
-            configuration.class = 'yellow';
-            configuration.icon = 'minus';
-            configuration.status = status.active;
+            pending(configuration);
+            $http.get('/activate' + configuration.name).then(function(success) {
+                active(configuration);
+            }, function(error) {
+                bugged(configuration);
+            });
         }
     };
 
@@ -55,21 +57,51 @@ function Configurations($scope, $http) {
 
     $scope.disable = function(configuration) {
         if (configuration.status && configuration.status === status.active) {
-            /* TODO */
-            configuration.class = 'green';
-            configuration.icon = 'plus';
-            configuration.status = status.available;
+            pending(configuration);
+            $http.get('/disable/' + configuration.name).then(function(success) {
+                available(configuration);
+            }, function(error) {
+                bugged(configuration);
+            });
         }
     };
 
     $scope.reload = function(configuration) {
         if (configuration.status && configuration.status === status.bugged) {
-            /* TODO */
+            pending(configuration);
+            $http.get('/activate' + configuration.name).then(function(success) {
+                active(configuration);
+            }, function(error) {
+                bugged(configuration);
+            });
         }
     };
 
     $scope.neitherActiveNorBugged = function(configuration) {
         return !(configuration.status === status.active || configuration.status === status.bugged);
     };
+
+    function available(configuration) {
+        configuration.class = 'green';
+        configuration.icon = 'plus';
+        configuration.status = status.available;
+    }
+
+    function active(configuration) {
+        configuration.class = 'yellow';
+        configuration.icon = 'minus';
+        configuration.status = status.active;
+    }
+
+    function bugged(configuration) {
+        configuration.class = 'orange';
+        configuration.icon = 'warning';
+        configuration.status = status.bugged;
+    }
+
+    function pending(configuration) {
+        configuration.class = 'basic loading';
+        configuration.icon = '';
+    }
 
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var fs = require('fs');
-var http = require('http');
+var express = require("express");
+var config = require('./config.json');
+var fs = require("fs");
+var http = require("http");
 var app = express();
 
-var configFolder = ".";
-var dockerHost = "";
+var docker = config.docker;
+var folders = config.folders;
 
-app.use(express.static('../client/'));
+app.use(express.static("../client/"));
 
-app.get('/list', function(req, res) {
-    fs.readdir(configFolder, function(err, files) {
+app.get("/list", function(req, res) {
+    fs.readdir("/config", function(err, files) {
         if (err)
             console.error(err);
 
@@ -20,19 +21,103 @@ app.get('/list', function(req, res) {
     });
 });
 
-app.get('/status/:config', function(req, res) {
+app.get("/status/:config", function(req, res) {
     var config = req.params.config;
-    // TODO
-    http.request(dockerHost + "/containers/" + config + "/json").end();
+
+    var options = {
+        host: docker.host,
+        path: "/containers/logstash-" + config + "/json",
+        port: docker.port,
+        method: "GET"
+    };
+
+    http.request(options, function(response) {
+        var statusCode = response.statusCode;
+        var content = "";
+        if (statusCode < 200 || statusCode > 299) {
+            res.json({status: "available"});
+        } else {
+            response.on("data", function(chunk) {
+                content += chunk;
+            });
+
+            response.on("end", function() {
+                var data = JSON.parse(content);
+
+                if (data.State && data.State.Status && data.State.Status === "running") {
+                    res.json({status: "active"});
+                } else {
+                    res.json({status: "bugged"});
+                }
+            });
+        }
+    }).end();
 });
 
-app.get('/read/:config', function(req, res) {
+app.get("/active/:config", function(req, res) {
     var config = req.params.config;
 
-    if (config.slice(-1) != '/')
-        config = '/' + config;
+    var options = {
+        host: docker.host,
+        path: "/containers/create?name=logstash-" + config,
+        port: docker.port,
+        method: "POST",
+        json: true,
+        body: JSON.stringify({
+            Image: "logstash:latest",
+            Mounts: [
+                {
+                    Source: folders.data,
+                    Destination: "/data"
+                },
+                {
+                    Source: folders.config,
+                    Destination: "/config"
+                }
+            ],
+            Cmd: ["logstash", "-f", "/config/" + config]
+        })
+    };
 
-    fs.readFile(configFolder + config, 'utf-8', function(err, data) {
+    http.request(options, function(response) {
+        var statusCode = response.statusCode;
+        var content = "";
+        if (statusCode < 200 || statusCode > 299) {
+            res.json({status: "active"});
+        } else {
+            res.json({status: "bugged"});
+        }
+    }).end();
+});
+
+app.get("/disable/:config", function(req, res) {
+    var config = req.params.config;
+
+    var options = {
+        host: docker.host,
+        path: "/containers/logstash-" + config + "?force=true",
+        port: docker.port,
+        method: "DELETE"
+    };
+
+    http.request(options, function(response) {
+        var statusCode = response.statusCode;
+        var content = "";
+        if (statusCode < 300) {
+            res.json({status: "available"});
+        } else {
+            res.json({status: "bugged"});
+        }
+    }).end();
+});
+
+app.get("/read/:config", function(req, res) {
+    var config = req.params.config;
+
+    if (config.slice(-1) != "/")
+        config = "/" + config;
+
+    fs.readFile("/config/" + config, "utf-8", function(err, data) {
         if (err)
             console.error(err);
 
